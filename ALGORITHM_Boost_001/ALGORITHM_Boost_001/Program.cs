09eaa20bb566064f892b422c13acc43013c760c5
GYALLERHORN <kjins1998@gmail.com>
2024-01-02 15:50:09 +0900

    약수의 개수와 덧셈 완료

--- ALGORITHM_Boost_001/ALGORITHM_Boost_001/Program.cs
+++ ALGORITHM_Boost_001/ALGORITHM_Boost_001/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Solution
+{
+    public int solution(int left, int right)
+    {
+        int answer = 0;
+
+        for (int i = left; i <= right; i++)
+        {
+            int aliquotCount = 0;
+            if (Math.Sqrt(i) % 1f == 0f) // 약수의 개수가 홀수인 경우는
+            {
+                aliquotCount++; // 따로 +1한다
+            }
+            for (int j = 1; j < Math.Sqrt(i); j++) // 약수 개수 구하기
+            {
+                if (i % j == 0) // 나누어떨어질 경우
+                {
+                    aliquotCount += 2; // +2
+                }
+            }
+            if (aliquotCount % 2 == 0)
+            {
+                answer += i;
+            }
+            else
+            {
+                answer -= i;
+            }
+        }
+        return answer;
+    }
+}
