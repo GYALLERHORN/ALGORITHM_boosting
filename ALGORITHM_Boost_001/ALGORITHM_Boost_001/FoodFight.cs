4f499dbf9e7be320f57db90b93c25c3479a15018
GYALLERHORN <kjins1998@gmail.com>
2024-01-03 15:19:23 +0900

    [서울에서 김서방 찾기] 완료

---
+++ ALGORITHM_Boost_001/ALGORITHM_Boost_001/FoodFight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public class FoodFight
+{
+    public string solution(int[] food)
+    {
+        string answer = "";
+
+        for (int i = 1; i < food.Length; i++)
+        {
+            int temp = food[i] / 2;
+            for (int j = 0; j < temp; j++)
+            {
+                answer += i;
+            }
+        }
+
+        char[] chaArr = answer.ToCharArray();
+        Array.Reverse(chaArr);
+
+        answer = answer + "0" + new string(chaArr);
+
+        return answer;
+    }
+}
