b391ce63c90331d1ebc75bd27865289d44a654b0
GYALLERHORN <kjins1998@gmail.com>
2024-01-05 17:53:47 +0900

    [할인 행사]

---
+++ ALGORITHM_Boost_001/ALGORITHM_Boost_001/MakeHamburger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class MakeHamburger
+{
+    public int solution(int[] ingredient)
+    {
+        int answer = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (int i in ingredient)
+        {
+            sb.Append(i);
+        }
+
+        string ingre = sb.ToString();
+
+        int breadPos = 0;
+
+        while (true)
+        {
+            if (ingre.Contains("1231"))
+            {
+                breadPos = ingre.IndexOf("1231");
+                for (int i = 0; i < 4; i++)
+                {
+                    ingre.Remove(ingre[breadPos]);
+                }
+                answer++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+
