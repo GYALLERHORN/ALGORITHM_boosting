eceeeb7c7b9b2432cd134d116570a03f68ec1cd2
GYALLERHORN <kjins1998@gmail.com>
2024-01-09 16:55:32 +0900

    [명예의 전당] 완료

---
+++ ALGORITHM_Boost_001/ALGORITHM_Boost_001/HallOfHonor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HallOfHonor
+{
+    public int[] solution(int k, int[] score)
+    {
+        var answer = new List<int>();
+
+        var hOH = new List<int>();
+        for (int i = 0; i < score.Length; i++)
+        {
+            hOH.Add(score[i]); // score에 한개씩 넣으면서
+            hOH = hOH.OrderByDescending(o => o).Take(k).ToList(); // 오름차순하고 최대 k개 뽑아내서
+            int lowestScore = hOH.Min(); // 그중 최솟값을 찾아낸다 
+            answer.Add(lowestScore);
+        }
+
+        return answer.ToArray();
+    }
+}
