93fc8eaf87d2e5b2c4ab2f272b8a4aac373e2957
GYALLERHORN <kjins1998@gmail.com>
2024-01-09 16:54:51 +0900

    [부족한 금액 찾기] 완료

---
+++ ALGORITHM_Boost_001/ALGORITHM_Boost_001/FindInsufficientMoney.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class FindInsufficientMoney
+{
+    public long solution(int price, long money, int count)
+    {
+        long totalFee = 0;
+        for (int i = 1; i <= count; i++)
+        {
+            totalFee += price * i;
+        }
+        if (totalFee <= money)
+        {
+            return 0;
+
+        }
+        return totalFee - money;
+    }
+}
